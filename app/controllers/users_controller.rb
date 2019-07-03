22fbf04d4009b2ae2fcc9c051de4c043cbf21a66
khushwanth <khushwanth.chhetri@gmail.com>
2019-07-03 18:51:14 +0600

    User Edit/Update Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,15 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
     end
 
     def update
-
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'Notification was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
     def show 
